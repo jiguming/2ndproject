994bac3416599ad44ca6e54c6588e21fdf95a297
jiguming <mshvc5@naver.com>
2025-06-10 18:54:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,7 @@ st.set_page_config(
 
 # --- 데이터: Astropy 객체로 재구성 ---
 # 이제 좌표는 SkyCoord 객체로, 크기는 단위(u.km)를 붙여 관리합니다.
-# 달의 좌표계(lunarographic)를 사용하고, 달의 반지름을 명시합니다.
-MOON_RADIUS = const.R_moon
+
 
 LOCATIONS_ASTRO = {
     "고요의 바다 (아폴로 11호 착륙지)": {
